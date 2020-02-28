655559d92c0a120ecfeaf5a42cbd8e644d179356
Hilário Araújo <hilario.rocha.araujo@gmail.com>
2020-02-28 01:59:22 +0000

    Initial commit

---
+++ Intro_Robot/Project/add.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class add
+{
+    public:
+    int compute(int no_1,int no_2);
+};
---
+++ Intro_Robot/class_inherit/ReadWrite/read_write_file.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ofstream out_file;
+    string data = "Robot_ID=0";
+    cout << "Write data:" << data << endl;
+    
+    //write the text file
+    out_file.open("Config.txt");
+    out_file << data << endl;
+    out_file.close();
+    
+    //read the text file
+    ifstream in_file;
+    in_file.open("Config.txt");
+    in_file >> data;
+    cout << "Read data:" << data << endl;
+    in_file.close();
+    return 0;
+}
